ecb083786366052e7853d517227a4292f3e645d6
rijeshtmg <68938831+rijeshtmg@users.noreply.github.com>
2023-03-23 15:46:10 +0545

    completion upto user home and statement

---
+++ Frontend/src/Component/Customer/Purchase/Purchase.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CNav from "../CNav/CNav";
+import "./Purchase";
+const Purchase = () => {
+  return (
+    <>
+      <CNav />
+      <h1>Purchase</h1>
+    </>
+  );
+};
+
+export default Purchase;
